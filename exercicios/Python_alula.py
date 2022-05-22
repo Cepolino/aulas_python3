03b7748b5954e7bf5da71b291ffd8e90db2983e8
Cepolino <104170617+Cepolino@users.noreply.github.com>
2022-05-22 12:52:17 -0300

    Update Python_alula.py
    Tornando o código mais agradável, nenhuma alteração no funcionamento.

--- exercicios/Python_alula.py
+++ exercicios/Python_alula.py
@@ -6,12 +6,12 @@ numero_secreto = 42
 total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
-    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    print(f "Tentativa {rodada} de {total_de_tentativas}")
     chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou " , chute_str)
     chute = int(chute_str)
 
-    if(chute < 1 or chute > 100):
+    if chute < 1 or chute > 100:
         print("Você deve digitar um número entre 1 e 100!")
         continue
 
@@ -19,13 +19,13 @@ for rodada in range(1, total_de_tentativas + 1):
     maior   = chute > numero_secreto
     menor   = chute < numero_secreto
 
-    if(acertou):
+    if acertou:
         print("Você acertou!")
         break
     else:
-        if(maior):
+        if maior:
             print("Você errou! O seu chute foi maior do que o número secreto.")
-        elif(menor):
+        elif menor:
             print("Você errou! O seu chute foi menor do que o número secreto.")
 
 print("Fim do jogo")
