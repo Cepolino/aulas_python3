c17d00c4c0bc7e6cd56c75cae3b2505ffb303b1f
Cepolino <104170617+Cepolino@users.noreply.github.com>
2022-05-26 09:42:23 -0300

    inclusao do argsparse

--- projetos/ls/ls.py
+++ projetos/ls/ls.py
@@ -1,14 +1,25 @@
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--hidden", help= 'Use O para diretorios ocultos', action ="store_true")
+args = parser.parse_args()
 
 lista = []
 pasta_atual = os.getcwd()
 arquivos = os.listdir(pasta_atual)
-for i in arquivos:
-    if not i.startswith("."):
-        lista.append(i)
 
-for i in  range(0, len(lista), 4):
-    linha = lista [i:i+4]
+if args.hidden:
+    for i in  range(0, len(arquivos), 4):
+        linha = arquivos [i:i+4]  
+        print  ("\t".join(linha)) 
+
+else:
+    for i in arquivos:
+        if not i.startswith("."):
+            lista.append(i)
+
+    for i in  range(0, len(lista), 4):
+        linha = lista [i:i+4]
     
-
-    print  ("\t".join(linha))
+        print  ("\t".join(linha))
